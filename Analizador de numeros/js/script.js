794781d47fdc4013eeaf40c8b92faf04edf17dd7
Gabriella-Aquino <gabyyaquino8@gmail.com>
2023-06-24 15:33:48 -0300

    analizador de numeros

--- Analizador de numeros/js/script.js
+++ Analizador de numeros/js/script.js
@@ -21,8 +21,41 @@ function inlist(n, l){
 
 function adicionar(){
    if(numvalido(num.value) && inlist(num.value, valores)){
-        window.alert('ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        tab.appendChild(item)
+        res.innerHTML = '' //para limpar a frase da tela enquanto tiver adcionando novos numero
    }else{
         window.alert('Preencha corretamente')
    }
+   //automatizar para apagar oq ta no input e não ter q clicar 
+   num.value=''
+   num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        document.window.alert('Adcione algum numero primeiro')
+    }
+    else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        for(pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        let media = Number(soma/tot)
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} numeros</p>
+        <p> O maior valor informado foi ${maior}</p>
+        <p>O menor numero informado foi ${menor}</p>
+        <p>A soma de todos os valores é ${soma}</p>
+        <p>A media de todos os valores é ${media}</p>`
+    }
 }
